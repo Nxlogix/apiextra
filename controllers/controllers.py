c3d6acdb33d626fefec9a1343eae96f2779cde07
Nxlogix <165151602+Nxlogix@users.noreply.github.com>
2025-04-26 13:26:38 -0600

    Update controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -87,3 +87,75 @@ def buscar_productos_por_nombre(nombre_producto):
         return jsonify({'msg': 'Error al buscar productos por nombre'}), 500
     
     
+    
+# Controlador: Editar Usuario
+def edit_usuario(usuario_id, nombre=None, email=None, password=None):
+    try:
+        usuario = Usuario.query.get(usuario_id)
+        if not usuario:
+            return jsonify({'msg': 'El usuario no existe'}), 404
+        
+        if nombre:
+            usuario.nombre = nombre
+        if email:
+            usuario.email = email
+        if password:
+            usuario.password = generate_password_hash(password)
+
+        db.session.commit()
+        return jsonify({'msg': 'Usuario actualizado con éxito', 'usuario': usuario.to_dict()}), 200
+    except Exception as e:
+        print(f"ERROR {e}")
+        return jsonify({'msg': 'Error al editar el usuario'}), 500
+    
+    # Controlador: Eliminar Usuario
+def delete_usuario(usuario_id):
+    try:
+        usuario = Usuario.query.get(usuario_id)
+        if not usuario:
+            return jsonify({'msg': 'El usuario no existe'}), 404
+        
+        db.session.delete(usuario)
+        db.session.commit()
+        return jsonify({'msg': 'Usuario eliminado con éxito'}), 200
+    except Exception as e:
+        print(f"ERROR {e}")
+        return jsonify({'msg': 'Error al eliminar el usuario'}), 500
+    
+# Controlador: Editar Producto
+def edit_producto(producto_id, nombre=None, precio=None, cantidad=None, categoria_id=None):
+    try:
+        producto = Producto.query.get(producto_id)
+        if not producto:
+            return jsonify({'msg': 'El producto no existe'}), 404
+
+        if nombre:
+            producto.nombre = nombre
+        if precio:
+            producto.precio = precio
+        if cantidad:
+            producto.cantidad = cantidad
+        if categoria_id:
+            categoria = Categoria.query.get(categoria_id)
+            if not categoria:
+                return jsonify({'msg': 'La categoría especificada no existe'}), 404
+            producto.categoria_id = categoria_id
+
+        db.session.commit()
+        return jsonify({'msg': 'Producto actualizado con éxito', 'producto': producto.to_dict()}), 200
+    except Exception as e:
+        print(f"ERROR {e}")
+        return jsonify({'msg': 'Error al editar el producto'}), 500
+# Controlador: Eliminar Producto
+def delete_producto(producto_id):
+    try:
+        producto = Producto.query.get(producto_id)
+        if not producto:
+            return jsonify({'msg': 'El producto no existe'}), 404
+        
+        db.session.delete(producto)
+        db.session.commit()
+        return jsonify({'msg': 'Producto eliminado con éxito'}), 200
+    except Exception as e:
+        print(f"ERROR {e}")
+        return jsonify({'msg': 'Error al eliminar el producto'}), 500
